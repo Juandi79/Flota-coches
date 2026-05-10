f32b25cbefc0a7f8c9d4f5295ca77dac36696801
Juandi79 <juandi1992@hotmail.com>
2026-05-10 20:21:34 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
 export function createClient() {
-  return createSupabaseClient(supabaseUrl, supabaseKey)
+  return createSupabaseClient(
+    'https://njpxvjjssmsxnwofbimv.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
